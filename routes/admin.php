24c07de300bf2ca3515466cb55485ca25c8c2654
ElsinwarAhmed <alsinwarahmed@gmail.com>
2021-06-15 13:07:31 +0300

    =setting and authentication redirect

--- routes/admin.php
+++ routes/admin.php
@@ -18,6 +18,7 @@ Route::group(
 
         Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function () {
             Route::get('/', 'DashboardController@index')->name('admin.dashboard');
+            Route::get('/logout', 'LoginController@logout')->name('admin.logout');
             Route::group(['prefix' => 'settings',], function () {
                 Route::get('shipping-method/{type}', 'SettingController@editShippings')->name('admin.settings.editShipping');
                 Route::post('shipping-method/{id}', 'SettingController@updateShippings')->name('admin.settings.updateShipping');
